7acb500070d8773512b8c048ef025383f6192f41
Yuri Neves <yurineves92@gmail.com>
2020-08-09 15:22:57 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#  Simple CRUD using PHP, PDO and MySQL
+
+- CRUD;
+- PDO Connection
+- Dump SQL
+
+## Ferramentas
+
+* [PHP] (https://www.php.net/)
+* [MySQL] (https://www.mysql.com/)
+* [PDO] (https://www.php.net/manual/pt_BR/book.pdo.php)
---
+++ template/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>PDO - Create a Record - PHP CRUD Tutorial</title>
+      
+    <!-- Latest compiled and minified Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+          
+</head>
+<body>
+    <!-- container -->
+    <div class="container">
